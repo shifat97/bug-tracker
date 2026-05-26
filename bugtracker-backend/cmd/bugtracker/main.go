9e7ed378602bd30c7fee87f9d3527c5e28815988
shifat97 <shifatbinreza2@gmail.com>
2026-05-26 21:56:22 +0600

    chore: add deployment URLs to CORS config and update gitignores to exclude fly-related files

--- bugtracker-backend/cmd/bugtracker/main.go
+++ bugtracker-backend/cmd/bugtracker/main.go
@@ -72,10 +72,12 @@ func createServer() *http.Server {
 			"http://localhost:3000",
 			"https://bugtracker-staging-jameswillett.fly.dev",
 			"https://bugtracker-jameswillett.fly.dev",
+			"https://bugtracker-shifatbinreza.fly.dev",
+			"https://bugtracker-staging-shifatbinreza.fly.dev",
 		},
-		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders: []string{"*"},
-		ExposedHeaders: []string{"Content-Length"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Content-Length"},
 		AllowCredentials: true,
 	})
 
